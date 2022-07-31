9ba108061a38088aa61adabfe42654c69f8fd67d
tanyaaga27 <110083650+tanyaaga27@users.noreply.github.com>
2022-07-31 16:09:55 +0530

    Arrays Declaration

---
+++ Arrays.java
@@ -0,0 +1,24 @@
+//ARRAYS IN JAVA
+//An object that contains elements of similar data type
+//Elements are stored in contiguous memory locations
+//It is index based
+//We can store primitive values or objects in an array
+
+class Arrays
+{
+	public static void main(String args[])
+	{
+		int a[];       //Declaration
+		a = new int[5] //Instatiation
+		a[0] = 100;    //Initialization
+		a[1] = 101;
+		a[2] = 200
+		a[3] = 350
+		a[4] = 525
+		
+		int b[] = {1,5,3,5}  //Declaration,Instantiation and Initialization
+		
+		int c[][] = new int[5][5]; //2-D Array
+		
+	}
+}
