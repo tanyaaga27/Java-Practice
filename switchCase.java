e2ccad54c398dbdc619057c3b333d5ec47943709
tanyaaga27 <110083650+tanyaaga27@users.noreply.github.com>
2022-08-02 14:26:12 +0530

    Switch Case

---
+++ switchCase.java
@@ -0,0 +1,17 @@
+class switchCase
+{
+	public static void main(String args[])
+	{
+		int b=5;
+		switch(b)
+		{
+			
+			case 1 : System.out.println("1");break;
+			case 2 : System.out.println("1");break;
+			case 3 : System.out.println("1");break;
+			case 4 : System.out.println("1");break;
+			default : System.out.println("Not Found");
+		}
+	}
+}
+			
